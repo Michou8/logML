8bed13cd3b18c797949f7bba4e035a9421c3c07c
HELAL Misu <michou.helal@gmail.com>
2016-02-29 00:37:56 +0100

    Add dbscan not operational yet

--- unsupervised.py
+++ unsupervised.py
@@ -62,7 +62,90 @@ class kmeans:
 			clusters,centroids = cl.clusters_(data,indices_c)
 					
 		return clusters,centroids
-#k = kmeans(k=3)
-#data = [[1,0],[1,1],[1,23],[2,1.2],[1,25]]
+def eps_near(eps,data,i):
+                res = []
+                for j in xrange(len(data)):
+                        if j!=i and distance(data[i],data[j])<=eps:
+                                res.append(j)
+                return res
+def extend_cluster(eps,minpts,data,P,c_clusters,clusters_):
+                eps_near_ = eps_near(eps,data,P)
+                if c_clusters not in clusters_:
+                        clusters_[c_clusters] = []
+                clusters_[c_clusters].append(P)
+                visited = []
+                for p_ in eps_near_:
+                        if p_ not in visited:
+                                visited.append(p_)
+                                p_eps_near_ = eps_near(eps,data,p_)
+                                if len(p_eps_near) >= minpts:
+                                        eps_near_ = eps_near_ + p_eps_near_
+                                size = len(clusters_)
+                                counter = 0
+                                for i in xrange(size):
+                                        if p_ not in clusters_:
+                                                counter += 1
+                                        else:
+                                                break
+                                if counter == size:
+                                        self.clusters[c_clusters].append(p_)
+                return self.clusters
+class dbscan:
+	#https://fr.wikipedia.org/wiki/DBSCAN
+	def __init__(self,eps=10,minpts=10):
+		self.eps = eps
+		self.minpts = minpts
+		self.clusters = {'NOISE':[]}
+	"""def eps_near(self,data,i):
+		eps = self.eps
+		res = []
+		for j in xrange(len(data)):
+			if j!=i and distance(data[i],data[j])<=eps:
+				res.append(j)
+		return res
+	"""
+	def extend_cluster(self,data,P,c_clusters):
+		eps = self.eps
+		minpts = self.minpts
+		eps_near_ = eps_near(eps,data,P)
+		if c_clusters not in self.clusters:
+			self.clusters[c_clusters] = []
+		self.clusters[c_clusters].append(P)
+		visited = []
+		for p_ in eps_near_:
+			if p_ not in visited:
+				visited.append(p_)
+				p_eps_near_ = eps_near(eps,data,p_)
+				if len(p_eps_near) >= minpts:
+					eps_near_ = eps_near_ + p_eps_near_
+				size = len(self.clusters)
+				counter = 0
+				for i in xrange(size):
+					if p_ not in self.clusters:
+						counter += 1
+					else:
+						break
+				if counter == size:
+					self.clusters[c_clusters].append(p_)		
+		return self.clusters
+	def fit(self,data):
+		c_clusters = 0
+		visited = []
+		cluster = {}
+		result = ['UNCLASSIFIED']*len(data)
+		for i in xrange(len(data)):
+			if i not in visited:
+				visited.append(i)
+				eps_near_ = eps_near(self.eps,data,i)
+				if len(eps_near_)< self.minpts:
+					result[i] = 'NOISE'
+				else:
+					c_luster += 1
+					cluster = dbscan().extend_cluser(data,i,eps_near_)
+		return cluster
+				
+db= dbscan()
+data = [[1,0],[1,1],[1,23],[2,1.2],[1,25]]
+print db.fit(data = data)
 #print len(data)
 #print k.fit(data)
