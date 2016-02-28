7370256ed4d5793726b8327e23ee7bf23b1653da
HELAL Misu <michou.helal@gmail.com>
2016-02-28 23:55:22 +0100

    Add kmeans clustering

--- analyze.py
+++ analyze.py
@@ -1,10 +1,16 @@
 import json
 import math
 import randomevent
+import unsupervised
 ########## First approach every meta has a value (No grouped)
 def encoder(events,meta_not_used = ['date','time','response_size','response_time','ip_adress']):
+	# It was not really efficient because we don't have any distance between two categories. What is the distance between 'get' and 'post'?
+	# ????? Not really relevant for me
 	encoder_ = {}
+	transform = []
+	order = events[0].keys()
 	for e in events:
+		tmp = []
 		for key in e:
 			if key not in meta_not_used:
 				if key not in encoder_:
@@ -12,7 +18,15 @@ def encoder(events,meta_not_used = ['date','time','response_size','response_time
 				else:
 					if e[key] not in encoder_[key]:
 						encoder_[key][e[key]] = len(encoder_[key])+1
-	return encoder_
+		for key in order:
+			if key not in meta_not_used:
+				tmp.append(encoder_[key][e[key]])
+		transform.append(tmp)	
+	return transform,encoder_
+####################################################
 
 events = randomevent.events(number=10000).evts()
-print encoder(events)
+data,encoder_ = encoder(events) 
+km = unsupervised.kmeans(k=3,n=10)
+print km.fit(data)
+#print encoder_
---
+++ unsupervised.py
@@ -0,0 +1,72 @@
+import math
+# Not necessary in future but recommand
+import numpy as np
+import random
+#######################################
+def distance(p,q,l=2):
+	res = 0.0
+	for i in xrange(len(p)):
+		res += abs(p[i]-q[i])**l
+	return math.sqrt(res)
+#from __future__ import division
+def mean(data):
+	res = [0]*len(data[0])
+	for d in data:
+		for i in xrange(len(d)):
+			res[i] += d[i]/float(len(data))
+	return res
+	#return np.mean(a)	
+class kmeans:
+	def __init__(self,k=2,n=100):
+		self.k = k
+		self.n_step = n
+	def random_centroid(self,data,k):
+		# generate random centroid for the 1st step
+		return random.sample(data,k)
+	def clusters_(self,data,indices):
+		clusters = {}
+		for key in indices:
+			if key not in clusters:
+				clusters[key] = []
+			for i in indices[key]:
+				clusters[key].append(data[i])
+		centroids = {}
+		for key in clusters:
+			centroids[key] = mean(clusters[key])
+		return clusters,centroids
+	
+	def fit(self,data):
+		# from https://en.wikipedia.org/wiki/K-means_clustering
+		cl = kmeans(k=self.k,n=self.n_step)
+		k = cl.k
+		n_step = cl.n_step
+		centroids = cl.random_centroid(data,k)
+		counter_ = 0
+		indices_c = {}
+		while counter_ < n_step:
+			counter_ += 1
+			indices_c = {}
+			for j in xrange(len(data)):
+				x_j = data[j]
+				for i in xrange(len(centroids)):
+					# compare the first centroid
+					if i not in indices_c:
+						indices_c[i] = []
+					m_i = centroids[i]
+					k_tmp = 0
+					for i_s in xrange(len(centroids)):
+						m_i_s = centroids[i_s]
+						if i_s!=i:
+							d_i = distance(m_i,x_j)
+							d_i_s = distance(m_i_s,x_j)
+							if d_i <= d_i_s:
+								k_tmp += 1
+					if k_tmp == k-1:
+						indices_c[i].append(j)
+			clusters,centroids = cl.clusters_(data,indices_c)
+					
+		return clusters,centroids
+#k = kmeans(k=3)
+#data = [[1,0],[1,1],[1,23],[2,1.2],[1,25]]
+#print len(data)
+#print k.fit(data)
